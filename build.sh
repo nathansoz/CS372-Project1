7156726167dec1aa0f9f35ddf7ab8923884ec92d
Nathan Sosnovske <nsosnov@microsoft.com>
2017-07-22 19:33:09 -0700

    General build stuff

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+export CC=clang
+export CXX=clang++
+
 pushd server
 mvn package
 popd
 
+if [[ ! -d client_build ]]; then 
+  mkdir client_build
+fi
+
+pushd client_build
+cmake ../client
+make
+
+popd
